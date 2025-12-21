6ef48bf27999dd0f80c26e9d5022ea1a73eab88b
Ninh <95522691+ninhnguyen01@users.noreply.github.com>
2025-12-20 21:56:33 -0800

    Add files via upload

---
+++ Getting Started-dev.java/src/Bicycle2.java
@@ -0,0 +1,11 @@
+public interface Bicycle2 {
+
+        //  wheel revolutions per minute
+        void changeCadence(int newValue);
+
+        void changeGear(int newValue);
+
+        void speedUp(int increment);
+
+        void applyBrakes(int decrement);
+    }
---
+++ Getting Started-dev.java/src/MyFirstJavaApp.java
@@ -0,0 +1,10 @@
+public class MyFirstJavaApp {
+    public static void main(String[] args) {
+        String name = "testName";
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming!");
+        System.out.println(42);
+        System.out.println("10 + 5 = " + (10 + 5));
+        System.out.println(("Hello, " + name));
+    }
+}
